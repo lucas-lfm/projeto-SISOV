345afdacb268f436fb5b51f0f9f4c9c3ad2d1369
JVKW <joaovictor.os064@gmail.com>
2024-10-16 22:41:26 -0300

    🚧 Estilização página de produção
    - Melhoria na página de produção

---
+++ frontend/public/js/functions.js
@@ -0,0 +1,13 @@
+function toggleMenu(model) {
+    const menu = document.getElementById("menu-lateral");
+    const isActive = menu.classList.contains("ativo");
+  
+    if (isActive) {
+      // Se o menu está ativo, então esconde o menu
+      menu.classList.remove("ativo");
+    } else {
+      // Se o menu não está ativo, então mostra o menu
+      menu.classList.add("ativo");
+    }
+  }
+  
